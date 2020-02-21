c02cc3f315c5f92d5d97352e48de4be0a5afba64
Linda <jokinenlindak@gmail.com>
2020-02-21 15:43:11 +0200

    added validation so that username has to be unique

--- application/user/forms.py
+++ application/user/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SelectField
+from wtforms import StringField, validators, SelectField, ValidationError
+from application.auth.models import User
+
+def validate_username(self, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+        raise ValidationError('That username is taken. Please choose another')
+
 
 class UserForm(FlaskForm):
     name = StringField("Nimi:", [validators.Length(min=2, max=50)])
-    username = StringField("Käyttäjänimi:", [validators.Length(min=2, max=50)])
+    username = StringField("Käyttäjänimi:", [validators.Length(min=2, max=50),validate_username])
     password = StringField("Salasana:", [validators.Length(min=6, max=20)])
     role = SelectField("Käyttäjärooli:", choices=[('ADMIN', 'ADMIN'), 
     ('USER', 'USER')])
  
     class Meta:
         csrf = False
+
